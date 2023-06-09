05af584002e4148d30abb6869e85d8427deb2382
Alois <sonya.shu2107@gmail.com>
2023-06-09 19:29:12 +0300

    Add all site

---
+++ main.js
@@ -0,0 +1,193 @@
+quiz_quest = [{"name":"Гетеро", "flag":"https://upload.wikimedia.org/wikipedia/commons/thumb/4/4e/Heterosexual_flag_%28black-grey-white%29.svg/200px-Heterosexual_flag_%28black-grey-white%29.svg.png", "desk":"Ну що тут казати, потуга лише до протилежної статті"},
+    {"name":"Гетеро ally", "flag":"https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Straight_Ally_flag.svg/330px-Straight_Ally_flag.svg.png", "desk":"Потуга лише до протилежної статі, але підтримують ЛГБТ+ рух"},
+    {"name":"ЛГБТ+", "flag":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSoYIEulsl9A6XHVhfaFmSrkO6xEUfY6FL3OkhTq10wvzJxJiKHwkYbzOGVVBwvd5hVa7E&usqp=CAU", "desk":"Загальнйи збір всіх сексуальностей та орієнтацій"},
+    {"name":"ЛГБТ+ прогресивні", "flag":"https://outrightinternational.org/sites/default/files/styles/coh_x_large/public/2022-11/new-pride-flag-01.jpg?itok=UA9jM-CV", "desk":"Оновлений прапор ЛГБТ+"},
+    {"name":"Геї", "flag":"https://cdn.britannica.com/35/232135-050-7DD3D7A7/Gay-Men-Pride-flag.jpg", "desk":"Сексуальний потяг до осіб своєї статі (чоловіки)"},
+    {"name":"Лесбіянки", "flag":"https://media.istockphoto.com/id/1370181615/vector/orange-pink-lesbian-flag-derived-from-the-pink-flag-circulated-on-social-media-in-2018.jpg?s=612x612&w=0&k=20&c=liCXqV_IVLpupYBnWvdrKIbz4ofoxVO0Y_hs6Xr7xLU=", "desk":"Сексуальний потяг до осіб своєї статі (жінки)"},
+    {"name":"Пан", "flag":"https://upload.wikimedia.org/wikipedia/commons/thumb/a/a2/Pansexuality_Pride_Flag.svg/1200px-Pansexuality_Pride_Flag.svg.png", "desk":"Гендерна індентичність партнера не грає ролі"},
+    {"name":"Пан", "flag":"https://static.wikia.nocookie.net/queer-community/images/1/12/New_pan_flag.png", "desk":"Гендерна індентичність партнера не грає ролі (оновлений стяг)"},
+    {"name":"Бі", "flag":"https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Bisexual_Pride_Flag.svg/1200px-Bisexual_Pride_Flag.svg.png", "desk":"Потяг до чоловічої та жіночої статі"},
+    {"name":"Асексуали", "flag":"https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/Asexual_Pride_Flag.svg/1280px-Asexual_Pride_Flag.svg.png", "desk":"Не виявляють будь-якого потягу (або в незначній кількості), але можуть мати романтичний потяг на відміну від аромантиків"},
+    {"name":"Трансгендери", "flag":"https://upload.wikimedia.org/wikipedia/commons/thumb/b/b0/Transgender_Pride_flag.svg/383px-Transgender_Pride_flag.svg.png", "desk":"Люди, які відчувають невідповідність біологічної станні з власним гендером"},
+    {"name":"Агендери", "flag":"https://upload.wikimedia.org/wikipedia/commons/thumb/e/ed/Agender_pride_flag.svg/2560px-Agender_pride_flag.svg.png", "desk":"Люди, що невідносять себе до жодної гендерної ідентичності"},
+    {"name":"Інтерсекс", "flag":"https://www.gendergp.com/wp-content/uploads/2022/10/2880px-Intersex_Pride_Flag_svg_4c6a20e5f49d5c42333f8434af0f8282_2000-980x653.png", "desk":"Особа, що має статеві ознаки, які не можуть бути чітко визначені як чоловічі чи жіночі"},
+    {"name":"Бігендери", "flag":"https://upload.wikimedia.org/wikipedia/commons/thumb/9/92/Bigender_Flag.svg/2560px-Bigender_Flag.svg.png", "desk":"Люди, котрі безвідносно до фізичної статі, можуть мати дві будь-які гендерні ідентичності, котрі здатні замінювати одна-одну"},
+    {"name":"Бігендери", "flag":"https://static.wikia.nocookie.net/gender/images/f/f2/Bigender.png", "desk":"Люди, котрі безвідносно до фізичної статі, можуть мати дві будь-які гендерні ідентичності, котрі здатні замінювати одна-одну"},
+    {"name":"Андрогінні", "flag":"https://static.miraheze.org/lgbtawiki/thumb/3/35/Androgyneflag1.png/1200px-Androgyneflag1.png", "desk":"Одночасна присутність в особи стереотипних для суспільства жіночих та чоловічих рис"},
+    {"name":"Гендерквір", "flag":"https://upload.wikimedia.org/wikipedia/commons/thumb/1/1f/Genderqueer_Pride_Flag.svg/768px-Genderqueer_Pride_Flag.svg.png", "desk":"Гендерна ідентичність відмінна від чоловічої чи жіночої (збірне поняття)"},
+    {"name":"Гендерфлюїд", "flag":"https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/Genderfluidity_Pride-Flag.svg/2560px-Genderfluidity_Pride-Flag.svg.png", "desk":"Нефіксована гендерна ідентичність, котра може змінюватись в залежності від обставин"},
+    {"name":"Поліамори", "flag":"https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Polyamory_Pride_Flag.svg/2560px-Polyamory_Pride_Flag.svg.png", "desk":"Система поглядів на кохання, яка допускає існування множиних любовних стосунків в один момент часу"},
+    {"name":"Демісексуали", "flag":"https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/Demisexual_Pride_Flag.svg/1280px-Demisexual_Pride_Flag.svg.png", "desk":"Будь-який сексуальний потяг з'являється лише після втсановлення емоційного зв'язку"},
+    {"name":"Полісексуали", "flag":"https://upload.wikimedia.org/wikipedia/commons/thumb/a/ae/Polysexuality_Pride_Flag.svg/1200px-Polysexuality_Pride_Flag.svg.png", "desk":"Потяг до багатьох, але не всіх гендерів"},
+    {"name":"Небінарні", "flag":"https://grpride.org/wp-content/uploads/2021/06/300px-Nonbinary_flag.svg.png", "desk":"Гендерна ідентичність, що не в ходить в бінарну (чоловік/жінка) ідентичність"},
+
+]
+quiz_ans = ["Гетеро", "Гетеро ally", "ЛГБТ+", "ЛГБТ+ прогресивні", "Геї", "Лесбіянки", "Пан", "Бі", "Асексуали", "Трансгендери", 
+            "Агендери", "Інтерсекс", "Бігендери", "Андрогінні", "Гендерквір", "Гендерфлюїд", "Поліамори", "Демісексуали", "Полісексуали", "Небінарні"]
+players_score = []
+players_right = []
+max_num = 6
+players_count = 1
+click_count = 0
+right_ans = {}
+quest_num = 0
+colour_arr = ["var(--red)","var(--orange)","var(--yellow)","var(--green)","var(--blue)","var(--purple)"]
+pl_choose = []
+
+document.addEventListener("DOMContentLoaded", function () {
+    quiz_quest = shuffleArray(quiz_quest)
+
+    subPlayBtn = document.getElementById("SubPlayBtn")
+    subPlayBtn.onclick = function (ev) {
+        tmp = document.getElementById("playerCount")
+        players_count = tmp.value < 1 ? 1: tmp.value > max_num ? max_num: tmp.value
+        tmp.value = players_count
+        tmp.disabled = true
+        tmp = document.getElementById("QuizDiv")
+        tmp.style.display = "inline"
+        tmp = document.getElementById("PlayersScoreHolder")
+        for (i=0; i<players_count; i++){
+            players_score.push(0)
+            pl_choose.push(-1)
+        }
+        ev.target.disabled = true
+    }
+
+    btnAnsArr = document.getElementsByClassName("BtnAnsVar")
+    for (i=0; i<btnAnsArr.length; i++){
+        btnAnsArr[i].onclick = function (ev){
+            ans = ev.target.value
+            numb = parseInt(ev.target.id.replace("BtnAnsV",""))
+            pl_choose[click_count] = numb
+            changeColorBtn(ev.target, numb)
+            if (ans==right_ans.name){
+                players_score[click_count] += 1
+                players_right.push(true)
+            }
+            else players_right.push(false)
+            click_count += 1
+            if (click_count >= players_count){
+                for (j=0; j<btnAnsArr.length; j++)
+                    btnAnsArr[j].disabled = true
+                tmp = document.getElementById("AnsverDiv")
+                tmp.style.display = "inline"
+                text_data = ""
+                for (j=0; j<players_count;j++){
+                    text_data += " Гр №"+(j+1)
+                    if (players_right[j]) text_data +=" правий <br>"
+                    else text_data +=" помилився <br>"
+                }
+                ans_text = document.getElementById("AnsverText")
+                ans_text.innerHTML = text_data
+                tmp = document.getElementById("NextBtn")
+                tmp.disabled = false
+            }
+
+        }
+    }
+
+    btnNext = document.getElementById("NextBtn")
+    btnNext.onclick = function (ev){
+        tmp = document.getElementById("AnsverDiv")
+        tmp.style.display = "none"
+        for (j=0; j<btnAnsArr.length; j++)
+            btnAnsArr[j].disabled = false
+        generate_quiz()
+        click_count = 0
+        players_right = []
+        pl_choose = []
+        for (i=0; i<players_count; i++){
+            pl_choose.push(-1)
+        }
+    }
+
+    generate_quiz()
+})
+
+function generate_quiz(){
+    if (quest_num < quiz_quest.length){
+        right_ans = quiz_quest[quest_num]
+        img = document.getElementById("FlagImg")
+        img.src = right_ans.flag
+        ans_name = document.getElementById("AnsverName")
+        ans_name.textContent = right_ans.name
+        ans_desc = document.getElementById("AnsverDesc")
+        ans_desc.textContent = right_ans.desk
+
+        tmp = [right_ans.name]
+        quiz_ans = shuffleArray(quiz_ans)
+        i=0
+        while (tmp.length<4){
+            if (quiz_ans[i] != tmp[0])
+                tmp.push(quiz_ans[i])
+            i++
+        }
+        tmp = shuffleArray(tmp)
+        for (i=0; i<tmp.length; i++){
+            btn = document.getElementById("BtnAnsV"+(i+1))
+            btn.value = tmp[i]
+            btn.style.background = "var(--lighter_main)"
+        }
+        
+        tmp = document.getElementById("NextBtn")
+        tmp.disabled = true
+
+        quest_num += 1
+    } else {
+        tmp = document.getElementById("AnsverDiv")
+        tmp.style.display = "inline"
+        ans_name = document.getElementById("AnsverText")
+        text_data = "Тест закінчено, результати:<br>"
+        for (i=0; i<players_count; i++){
+            text_data += " гр №"+(i+1)+" - "+players_score[i]+" балів <br>"
+        }
+        ans_name.innerHTML = text_data
+
+        tmp = document.getElementById("QuizDiv")
+        tmp.style.display = "none"
+
+        tmp = document.getElementById("NextBtn")
+        tmp.disabled = true
+
+        ans_name = document.getElementById("AnsverName")
+        ans_name.textContent = ""
+        ans_desc = document.getElementById("AnsverDesc")
+        ans_desc.textContent = ""
+
+    }
+}
+
+function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    return array
+}
+
+function changeColorBtn(btn, numb) {
+    col_ind = []
+    for (i=0; i<pl_choose.length; i++){
+        if (pl_choose[i] == numb)
+            col_ind.push(i)
+    }
+    text_grad = "linear-gradient( 90deg, "
+    for (i=0; i<col_ind.length; i++){
+        text_grad += colour_arr[col_ind[i]]
+        divid =  col_ind.length==1 ? 1 : col_ind.length-1 
+        text_grad += " "+100/divid*i+"%" 
+        if (i<col_ind.length-1)
+            text_grad +=", "
+    }
+    if (col_ind.length <= 1){
+        text_grad += ", "+colour_arr[col_ind[0]]
+        text_grad += " 100%" 
+    }
+
+    text_grad +=")"
+
+    btn.style.backgroundImage = text_grad
+}
+
